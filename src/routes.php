65145bf2f21edd14be3b23396008f00ecc227209
erdodif <erdodif@gmail.com>
2021-12-08 13:22:56 +0100

    token generálása

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use Hu\Petrik\Token;
 use Hu\Petrik\User;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\RequestInterface as Request;
@@ -23,5 +24,21 @@ return function (App $app) {
         $user->save();
         $res->getBody()->write($user->toJson());
         return $res->withHeader('Content-Type', 'application/json')->withStatus(RESPONSE_CREATED);
+    });
+    $app->post("/login",function(Request $req, Response $res){
+        $loginData = json_decode($req->getBody(), true);
+        $email = $loginData["email"];
+        $password = $loginData["password"];
+        $user = User::where("email",$email)->firstOrFail();
+        if(!password_verify($password,$user->password)){
+            throw new Exception("Hibás email vagy jelszó!");
+        }
+        $token = new Token();
+        $token->user_id = $user->id;
+        $token->token = bin2hex(random_bytes(64));
+        //pl keresés hogy ne legyen ütközés
+        $token->save();
+        $res->getBody()->write(json_encode(["email"=>$email, "token"=>$token->token]));
+        return $res->withHeader('Content-Type','application/json')->withStatus(RESPONSE_OK);
     });
 };
